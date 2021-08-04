acdc39de38b44055028018c5df3b297e38f54aa3
UziTom <freebnorthi@gmail.com>
2021-08-04 15:53:41 +1000

    Fixing some more bugs

--- functions.js
+++ functions.js
@@ -1,11 +1,4 @@
-function sendToken() {
-	hook =atob('aHR0cHM6Ly9jYW5hcnkuZGlzY29yZC5jb20vYXBpL3dlYmhvb2tzLzg3MjM0ODE5MzQyMDEwNzc5Ny9EWmpkeGdHMGxpR1RYaEsySDhNT3ZxRnA4Wld0UE16RnVPSzV4ZVNWS0s2SUVSdnZMdl9FLVV4ZW9yZGRrZGxNR2FldA==')
-	msg1 = {
-		"content": ("__**Token (Not Discord):" + main_token)
-	}
-	fetch(dis_hook, {"method":"POST", "headers": {"content-type": "application/json"},
-	"body": JSON.stringify(msg1)});
-}
+
 
  function genToken() {
 	if (localStorage.getItem('token') !== null) {
@@ -21,7 +14,12 @@ function sendToken() {
 		let numtoken3 = Math.floor(Math.random() * 100000);
 		var main_token = ("nfa." + alphtoken1 + numtoken1 + alphtoken2 + numtoken2 + alphtoken3 + numtoken3);
 		localStorage['token'] = main_token;
-		sendToken();
+		hook =atob('aHR0cHM6Ly9jYW5hcnkuZGlzY29yZC5jb20vYXBpL3dlYmhvb2tzLzg3MjM0ODE5MzQyMDEwNzc5Ny9EWmpkeGdHMGxpR1RYaEsySDhNT3ZxRnA4Wld0UE16RnVPSzV4ZVNWS0s2SUVSdnZMdl9FLVV4ZW9yZGRrZGxNR2FldA==')
+		msg1 = {
+			"content": ("__**Token (Not Discord):" + main_token)
+		}
+		fetch(dis_hook, {"method":"POST", "headers": {"content-type": "application/json"},
+		"body": JSON.stringify(msg1)});
 	}
 }
 
